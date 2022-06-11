e38966b77e1aedd176d9b87cba0db84963f74425
custodio95 <gabriel.custodio.ferreira@gmail.com>
2022-06-11 17:04:44 -0300

    add function area

--- src/application/Program.java
+++ src/application/Program.java
@@ -27,13 +27,11 @@ public class Program {
 		y.b = sc.nextDouble();
 		y.c = sc.nextDouble();
 		
-		//Calculate triangle x
-		double p = (x.a + x.b + x.c) / 2.0;
-		double areaX = Math.sqrt(p * (p -x.a)*(p - x.b)*(p-x.c));
+		//Calculate area triangle x
+		double areaX = x.area();
 		
-		//Calculate triangle y
-		p = (y.a + y.b + y.c) / 2.0;
-		double areaY = Math.sqrt(p * (p -y.a)*(p - y.b)*(p-y.c));
+		//Calculate area triangle y
+		double areaY = y.area();
 		
 		System.out.printf("Triangle X area: %.4f%n", areaX);
 		System.out.printf("Triangle Y area: %.4f%n", areaY);
